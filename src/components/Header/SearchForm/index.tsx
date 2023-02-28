3f951aec26a0a9a4e6834aab3acb699930007e3c
toledomg <toledomg@yahoo.com.br>
2023-02-28 18:21:04 -0300

    :sparkles: feat: iniciando campo search (extra rubrica)

--- src/components/Header/SearchForm/index.tsx
+++ src/components/Header/SearchForm/index.tsx
@@ -1,14 +1,31 @@
 import { MdSearch } from 'react-icons/md';
+import { useContext, useState } from 'react';
 import { StyledSearchForm } from './style';
+import { CartContext } from '../../../providers/CartContext';
 import { StyledButton } from '../../../styles/button';
 
-const SearchForm = () => (
-  <StyledSearchForm>
-    <input type='text' placeholder='Digitar pesquisa' />
-    <StyledButton type='submit' $buttonSize='medium' $buttonStyle='green'>
-      <MdSearch />
-    </StyledButton>
-  </StyledSearchForm>
-);
+const SearchForm = () => {
+  const { search, setSearch, cartList } = useContext(CartContext);
+  const [searchValue, setSearchValue] = useState([]);
+
+  const searchProducts = () => {
+    if (searchValue) {
+      setSearchValue();
+    }
+  };
+
+  return (
+    <StyledSearchForm>
+      <input
+        type='text'
+        placeholder='Digitar pesquisa'
+        onChange={(event) => setSearchValue(event.target.value)}
+      />
+      <StyledButton type='submit' $buttonSize='medium' $buttonStyle='green'>
+        <MdSearch />
+      </StyledButton>
+    </StyledSearchForm>
+  );
+};
 
 export default SearchForm;
