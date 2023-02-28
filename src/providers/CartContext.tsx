0ad6c2e5930f1c166d1b468dff7cabee4cacb2cf
toledomg <toledomg@yahoo.com.br>
2023-02-28 13:36:38 -0300

    :sparkles: feat: implementado persistir cart-localStorage

--- src/providers/CartContext.tsx
+++ src/providers/CartContext.tsx
@@ -23,8 +23,10 @@ export const CartProvider = ({ children }: IDefaultProviderProps) => {
   const localCart = localStorage.getItem('@HamburgerKenzie');
   const [isOpenModal, setIsOpenModal] = useState(false);
 
-  const [cartList, setCartList] = useState([] as IProductCart[]);
-  const [search, setSearch] = useState<IProduct[]>([]);
+  const [cartList, setCartList] = useState<IProductCart[]>(
+    localCart ? JSON.parse(localCart) : []
+  );
+  const [search, setSearch] = useState<IProduct[]>();
 
   const [showModal, setShowModal] = useState(false);
   const modalShow = () => {
