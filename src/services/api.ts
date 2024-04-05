e2e70eaa9a0af561d83fb81e910f756455c5cbb1
Alexsandro Tolêdo <toledomg@yahoo.com.br>
2024-04-05 16:51:45 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://hamburgueria-kenzie-v2.herokuapp.com',
+  // baseURL: 'https://hamburgueria-kenzie-v2.herokuapp.com',
+  baseURL: 'https://hbv2.alextoledo.com.br',
   responseType: 'json',
   timeout: 5000,
 });
