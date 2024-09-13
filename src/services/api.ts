7431dc35d5c3cb986863b94a51ea6ac333069a97
toledomg <toledomg@yahoo.com.br>
2024-09-13 11:59:47 -0300

    feat: implemented dotenv in services

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,21 @@
 import axios from 'axios';
 
+const localUrl = import.meta.env.VITE_API_LOCAL_URL;
+const deployUrl = import.meta.env.VITE_API_DEPLOY_URL;
+
+// Use a variável VITE_ENV para controlar o ambiente ou `NODE_ENV` para produção
+const baseURL =
+  import.meta.env.VITE_ENV === 'development' ||
+  import.meta.env.NODE_ENV === 'development'
+    ? localUrl
+    : deployUrl;
+
 export const api = axios.create({
-  // baseURL: 'https://hamburgueria-kenzie-v2.herokuapp.com',
-  baseURL: 'https://hbv2.alextoledo.com.br',
-  responseType: 'json',
+  baseURL: baseURL,
   timeout: 5000,
+  responseType: 'json',
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+  },
 });
